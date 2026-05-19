4e64917fd8c5397692ac72d057322820be35932f
Swayanshu Sekhar <swayanshupanda650@gmail.com>
2026-05-19 14:26:26 +0530

    2nd commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -175,6 +175,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <meta name="google-adsense-account" content="ca-pub-4170417139767375" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
